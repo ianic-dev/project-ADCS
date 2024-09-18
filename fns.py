38fe4165a144faa0ba25e255212fb267f3f5d32a
ianic-dev <ian@ianic.dev>
2024-09-18 17:26:46 +0200

    wrapping up the session

--- fns.py
+++ fns.py
@@ -3,3 +3,4 @@ import math as m
 def get_orbital_period(grav_param: float, semimajoraxis: float) -> float:
     period = 2 * m.pi * m.sqrt(semimajoraxis**3/grav_param)
     return period
+
--- inputvars.py
+++ inputvars.py
@@ -1,4 +1,11 @@
 mercury_radius = 2439.7e3
-final_sma = mercury_radius + 750e3
-eclipes_fraction = 0.277
 mercury_grav_parameter = 2.20329e13
+
+eclipes_fraction = 0.277
+
+final_sma = mercury_radius + 750e3
+max_disturbance_torque = "idk"
+
+margin_factor = 0.4
+
+
